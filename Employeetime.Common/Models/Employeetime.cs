40aa5688348058cca230aca9775711312c97a952
CristinaMarin490 <78991431+CristinaMarin490@users.noreply.github.com>
2021-08-30 22:40:17 -0500

    Add entity

---
+++ Employeetime.Common/Models/Employeetime.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Employeetime.Common.Models
+{
+    public class Employeetime
+    {
+        public int Id { get; set; }
+
+        public DateTime EntryDate { get; set; }
+
+        public string Type { get; set; }
+
+        public bool Consolidate { get; set; }
+    }
+}
---
+++ Employeetime.Functions/Entities/EmployeetimeEntity.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employeetime.Functions.Entities
+{
+    public class EmployeetimeEntity : TableEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime EntryDate { get; set; }
+
+        public string Type { get; set; }
+
+        public bool Consolidate { get; set; }
+    }
+}
